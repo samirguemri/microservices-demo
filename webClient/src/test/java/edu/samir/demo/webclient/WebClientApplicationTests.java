9763db3da16e88a200a2def5e4304388d53d9fbc
Samir <samir.guemri@gmx.fr>
2021-06-04 12:07:11 +0200

    init commit for web-client project

---
+++ webClient/src/test/java/edu/samir/demo/webclient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.samir.demo.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
